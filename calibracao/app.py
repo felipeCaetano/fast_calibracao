1aade1447a49d805a31aee89da530b1f1d586511
Felipe Caetano <felipecmelo@gmail.com>
2024-12-26 12:04:38 -0300

    Implementando Schemas

--- calibracao/app.py
+++ calibracao/app.py
@@ -1,7 +1,14 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from calibracao.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    ...
